4502476f446f8de6f34dc8411427118a6b0b82dd
ahazimeh <hazimeh95@gmail.com>
2022-12-01 14:49:36 +0200

    validation

--- src/helpers/validation.ts
+++ src/helpers/validation.ts
@@ -1,21 +1,18 @@
 import { Response } from "express";
 
-const validation = (result: Promise<void>, res: Response) => {
-  return result
-    .then(() => {
-      res.send({ success: true });
-    })
-    .catch((error: any) => {
-      console.log("_res", error);
-      let errors: any = {};
-      Object.keys(error.errors).forEach((key) => {
-        errors[key] = error.errors[key].message;
-      });
-
-      return res.status(400).send({
-        success: false,
-        errors,
-      });
+const validation = async (result: Promise<void>, res: Response) => {
+  try {
+    await result;
+    return res.send({ success: true });
+  } catch (error) {
+    let errors: any = {};
+    Object.keys(error.errors).forEach((key) => {
+      errors[key] = error.errors[key].message;
     });
+    return res.status(400).send({
+      success: false,
+      errors,
+    });
+  }
 };
 export default validation;
