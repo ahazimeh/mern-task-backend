6944d8f63fbfc7abec4e25818c466ae123073960
ahazimeh <hazimeh95@gmail.com>
2022-12-03 20:01:27 +0200

    signup fixed

--- src/controllers/authentication.ts
+++ src/controllers/authentication.ts
@@ -31,30 +31,33 @@ export const signup = function (
       .send({ error: "You must provide email and password " });
   }
   // See if a user with a given email exists
-  User.findOne({ email }, function (err: Error, existingUser: typeof User) {
-    if (err) {
-      return next(err);
-    }
-
-    // If a user with email does exist, return an error
-    if (existingUser) {
-      return res.status(422).send({ error: "Email is in user" });
-    }
-
-    // If a user with email does NOT exist, create and save user record
-    const user = new User({
-      email,
-      password,
-    });
-
-    user.save(function (err: Error) {
+  return User.findOne(
+    { email },
+    function (err: Error, existingUser: typeof User) {
       if (err) {
         return next(err);
       }
 
-      // Respond to request that indicating the user was created
-      res.json({ token: tokenForUser(user) });
-    });
-  });
-  return next();
+      // If a user with email does exist, return an error
+      if (existingUser) {
+        return res.status(422).send({ error: "Email is in user" });
+      }
+
+      // If a user with email does NOT exist, create and save user record
+      const user = new User({
+        email,
+        password,
+      });
+
+      user.save(function (err: Error) {
+        if (err) {
+          return next(err);
+        }
+
+        // Respond to request that indicating the user was created
+        res.json({ token: tokenForUser(user) });
+      });
+    }
+  );
+  // return next();
 };
